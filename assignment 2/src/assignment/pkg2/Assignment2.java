f3ad08fcb1590d92eacaf266f7cb39ac198c8663
SaitoVN <Tuanleqn2006@gmail.com>
2025-08-14 12:01:02 +0700

    Add files via upload

---
+++ assignment 2/src/assignment/pkg2/Assignment2.java
@@ -0,0 +1,12 @@
+public class Assignment2 {
+    public static void main(String[] args) {
+        System.out.println("1. 9 / 5 = " + (9.0 / 5));
+        System.out.println("2. 695 % 20 = " + (695 % 20));
+        System.out.println("3. 7 + 6 * 5 = " + (7 + 6 * 5));
+        System.out.println("4. 7 * 6 + 5 = " + (7 * 6 + 5));
+        System.out.println("5. 248 % 100 / 5 = " + ((248 % 100) / 5.0));
+        System.out.println("6. 6 * 3 - 9 / 4 = " + (6 * 3 - 9.0 / 4));
+        System.out.println("7. (5 - 7) * 4 = " + ((5 - 7) * 4));
+        System.out.println("8. 6 + (18 % (17 - 12)) = " + (6 + (18 % (17 - 12))));
+    }
+}
